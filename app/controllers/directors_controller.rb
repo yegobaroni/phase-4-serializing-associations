21727a83f380edd4dfe9e861349afd95bcb66c34
yego <yegokipkemoi254@gmail.com>
2022-11-25 19:59:44 +0300

    commit tree cf8f6284f00c6e2a9eb6338dc554a175c0f41e3d

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,14 +1,14 @@
 class DirectorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
+ def index
     directors = Director.all
-    render json: directors
+    render json: directors, include: ['movies', 'movies.reviews']
   end
 
   def show
     director = Director.find(params[:id])
-    render json: director
+    render json: director, include: ['movies', 'movies.reviews']
   end
 
   private
